e97f7ea69058d776c34450f3c0cc3528af5053cc
fredcaine <fefkwt@gmail.com>
2026-04-20 23:16:58 +0300

    main file commit

--- The Soul of Etria.py
+++ The Soul of Etria.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46491c2dda2de3557aa84d904940929318737f6eede63be6c3f6d7016005f1e4
-size 129626611
+oid sha256:b4ee6f3b584c2ac947e767acfc3047b551d505c8f170492b4da6031ba2bb309a
+size 129629599
